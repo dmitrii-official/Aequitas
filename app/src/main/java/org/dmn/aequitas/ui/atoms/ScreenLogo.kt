8b6f178eb2c28e390cff4caca0e07951d6bec7e6
dmitrii-official <dmitrii.murzin+official@gmail.com>
2026-06-26 11:56:09 -0400

    minor fix

--- app/src/main/java/org/dmn/aequitas/ui/atoms/ScreenLogo.kt
+++ app/src/main/java/org/dmn/aequitas/ui/atoms/ScreenLogo.kt
@@ -2,10 +2,8 @@ package org.dmn.aequitas.ui.atoms
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import org.dmn.aequitas.R
